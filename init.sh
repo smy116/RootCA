0e8f981e3d3f80aa104d938d2cb11d4b6aac03cf
SMY <smy116@users.noreply.github.com>
2023-12-27 16:40:32 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-export LC_ALL=C
-export LANG=en_US.UTF-8
-export LANGUAGE=en_US.UTF-8
-
 osRelease=""
 isChina=0
 
 publicKey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCaK2pVOdTT8POACgWPJ52adBHmtgx462N/Fn22r7srjYJrvPZH+zOofEbvAUTurjXAL4RD+Lc78cZRCm+CYifpweA9gmS0UTo5PaFLTGruOCzMZHfpGDnSqw5CHMS00xQx1Yozq5e+x6cNxtM0ZdQYy0x1pkhdI+W+mZIUCMaM07469At2YNqxcIcTwIXZgAR2Bd9WiogDHvVoDo1yoQBIXNN+dpA23BJQGE+hNbnasUACI6SfN6HWok2rdyQAXvzuVa7QWjhjqT0HmST8l4f4vJ6DnCKP/yZSjoO0vc72IyHduLk9qVhjhsOehLoi7AXdsX5Ab7HggTZ00Uln+ZBz root@iZ2894t29fxZ"
-
 
 # SMY Root Certification Authority RSA
 read -r -d '' RSA_Content << 'EOF'
@@ -64,7 +59,6 @@ AwMDaAAwZQIwWOC+QllECy76guWZNdQSDXO5sSUlo1JBwQVTbwDrtF2ABB7ptjgh
 aSsoBsICQmAdfnx+
 -----END CERTIFICATE-----
 EOF
-
 
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "This script must be run as root" 
