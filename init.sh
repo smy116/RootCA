1879aa8b8412ed8b4f2ec586b61f68f75a5e456a
SMY <smy116@users.noreply.github.com>
2024-01-29 15:51:56 +0800

    Update ssh key to ed25519

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 osRelease=""
 isChina=0
 
-publicKey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCaK2pVOdTT8POACgWPJ52adBHmtgx462N/Fn22r7srjYJrvPZH+zOofEbvAUTurjXAL4RD+Lc78cZRCm+CYifpweA9gmS0UTo5PaFLTGruOCzMZHfpGDnSqw5CHMS00xQx1Yozq5e+x6cNxtM0ZdQYy0x1pkhdI+W+mZIUCMaM07469At2YNqxcIcTwIXZgAR2Bd9WiogDHvVoDo1yoQBIXNN+dpA23BJQGE+hNbnasUACI6SfN6HWok2rdyQAXvzuVa7QWjhjqT0HmST8l4f4vJ6DnCKP/yZSjoO0vc72IyHduLk9qVhjhsOehLoi7AXdsX5Ab7HggTZ00Uln+ZBz root@iZ2894t29fxZ"
+publicKey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETbOuKJEi5BUJXkCopshD/dfAKTOphKM9fqffCH5v+Y SMY"
 
 # SMY Root Certification Authority RSA
 read -r -d '' RSA_Content << 'EOF'
